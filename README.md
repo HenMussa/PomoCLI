d1759e7f65ca359b1c720d623513ee37c3b54b6e
HenMussa <umcarinha94@gmail.com>
2022-04-06 21:53:15 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PomoCLI
+PomoCLI is a pomodoro timer that runs on the shell of your Unix-Like system.
+Learn more about the pomodoro technique: https://en.wikipedia.org/wiki/Pomodoro_Technique
+
+# Usage
+The flag "--times" is optional, the value is 1 by default.
+Don't worry about how manye time you have left, the software may notificate you. 
---
+++ src/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"flag"
+	"github.com/gen2brain/beeep"
+)
+
+func main() {
+	// Define the flag --times
+	timesFlag := flag.Int("times", 1, "defines how many times you want to run the pomocli timer, after 4 pomodoros the break time may increase")
+
+	flag.Parse()
+
+	for i := 1; i <= *timesFlag; i++ {
+		// Starts the pomodoro timer and notifies the user 
+		fmt.Printf("Pomodoro %v started\n", i)
+		beeep.Notify("PomoCLI", "Pomodoro started", "pomocli/src/assets/clock.png")
+		fmt.Println("25 minutes of focus!")
+		timer := time.NewTimer(25 * time.Minute)
+
+		// When the timer for focus ends, the break timer starts
+		<-timer.C
+		brTime := 5 * time.Minute
+		if i > 4 {
+			brTime = 15 * time.Minute
+		}
+		fmt.Printf("Break Time of %v!\n", brTime)
+		beeep.Notify("PomoCLI", "Break Time!", "pomocli/src/assets/break.jpg")
+		breakTimer := time.NewTimer(brTime)
+
+		<-breakTimer.C
+		fmt.Println("Break time finished!")
+		fmt.Println("============================================")
+	} 
+}
